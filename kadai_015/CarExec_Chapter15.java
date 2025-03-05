2addac3ced50cf215e6b66947ef02d0610477eef
tana-shi <shingo_tana626@icloud.com>
2025-03-05 21:37:22 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,16 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// インスタンス生成
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		// ギアを3に変更
+		car.getGearChange(3);
+		
+		// 速度を表示
+		car.run();
+
+	}
+}
